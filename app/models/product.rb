f2c9d0e39446e2f11bb37f51c1404c8f65060144
suhelrana10 <sohelmbstucse@gmail.com>
2021-11-25 18:29:08 +0600

    validation model title length

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 
   validates :title, :description, :image_url, presence:{message: "%{value} must be present"}
-  validates :title, uniqueness:{case_sensitive: false}
+  validates :title, uniqueness:{case_sensitive: false},length:{in: 5..20}
   validates :image_url, allow_blank: true,format:{with: %r{\.(gif|jpeg|png|jpg)\z}i,
                                                   message: "must be image extenssinom"}
   validates :price ,numericality: {greater_than: 0.0}
