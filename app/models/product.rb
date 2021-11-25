a7827ae018febd3f4adff16996e9a5f36b315a2e
suhelrana10 <sohelmbstucse@gmail.com>
2021-11-25 18:23:30 +0600

    validation product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence:{message: "%{value} must be present"}
+  validates :title, uniqueness:{case_sensitive: false}
+  validates :image_url, allow_blank: true,format:{with: %r{\.(gif|jpeg|png|jpg)\z}i,
+                                                  message: "must be image extenssinom"}
+  validates :price ,numericality: {greater_than: 0.0}
+
 end
